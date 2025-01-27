e4075d0da806e9f14d83e542465a2ea0d53e2eff
taimats <beo8939@gmail.com>
2025-01-28 00:18:21 +0900

    修正:recordコンポーネントのuiを修正

--- src/components/recordItems/recordItems.tsx
+++ src/components/recordItems/recordItems.tsx
@@ -2,52 +2,46 @@ export const RecordItems = () => {
     // データをフェチする
     return (
         <>
-        <div className="space-y-8">
-            <div className="bg-black rounded-md mt-4 mx-48">
-                <div className="flex items-center justify-center gap-4 pl-4 py-4">
-                    <div className="text-lg text-white font-bold">購入額</div>
-                    <div className="text-xl text-white font-bold">金額</div>
-                    <div className="relative right-2 text-lg text-white font-bold">円</div>
+        <div className="space-y-16 flex-col item-center justify-center gap-8 mt-8 mb-8">
+            <div className="space-y-4 flex-col flex-nowrap items-center justify-center bg-black rounded-md min-w-lg mx-32 py-4">
+                <div className="flex justify-center items-center gap-4">
+                    <div className="text-white text-2lg">購入額</div>
+                    <div className="text-white text-2xl">金額</div>
+                    <div className="text-white text-lg">円</div>
                 </div>
-
-                <div className="relative bg-white rounded-md mx-48 mb-32">
-                    <div className="flex items-center justify-center gap-4">
-                        <div className="text-sm">読了済</div>
-                        <div className="text-sm">金額</div>
-                        <div className="relative right-2 text-sm">円</div>
-                    </div>
+                
+                <div className="flex justify-center bg-white min-w-xl gap-4 px-4 py-2">
+                    <div>読了済</div>
+                    <div>金額</div>
+                    <div>円</div>
                 </div>
             </div>
 
-            <div className="bg-black rounded-md mx-48">
-                <div className="flex items-center justify-center gap-4 pl-4 py-4">
-                    <div className="text-lg text-white font-bold">購入冊数</div>
-                    <div className="text-xl text-white font-bold">何冊</div>
-                    <div className="relative right-2 text-lg text-white font-bold">冊</div>
+            <div className="space-y-4 flex-col flex-nowrap items-center justify-center bg-black rounded-md min-w-lg mx-32 py-4">
+                <div className="flex justify-center items-center gap-4">
+                    <div className="text-white text-2lg">冊数</div>
+                    <div className="text-white text-2xl">数量</div>
+                    <div className="text-white text-lg">冊</div>
                 </div>
-
-                <div className="relative bg-white rounded-md mx-48 mb-32">
-                    <div className="flex items-center justify-center gap-4">
-                        <div className="text-sm">読了済</div>
-                        <div className="text-sm">何冊</div>
-                        <div className="relative right-2 text-sm">冊</div>
-                    </div>
+                
+                <div className="flex justify-center bg-white min-w-xl gap-4 px-4 py-2">
+                    <div>読了済</div>
+                    <div>数量</div>
+                    <div>冊</div>
                 </div>
             </div>
 
-            <div className="bg-black rounded-md mx-48">
-                <div className="flex items-center justify-center gap-4 pl-4 py-4">
-                    <div className="text-lg text-white font-bold">総ページ数</div>
-                    <div className="text-xl text-white font-bold">何ページ</div>
-                    <div className="relative right-2 text-lg text-white font-bold">ページ</div>
+            <div className="space-y-4 flex-col flex-nowrap items-center justify-center bg-black rounded-md min-w-lg mx-32 py-4">
+                <div className="flex justify-center items-center gap-4">
+                    <div className="text-white text-2lg">ページ数</div>
+                    <div className="text-white text-2xl">数量</div>
+                    <div className="text-white text-lg">ページ</div>
                 </div>
-
-                <div className="relative bg-white rounded-md mx-48 mb-32">
-                    <div className="flex items-center justify-center gap-4">
-                        <div className="text-sm">読了済</div>
-                        <div className="text-sm">何ページ</div>
-                        <div className="relative right-2 text-sm">ページ</div>
-                    </div>
+                
+                <div className="flex justify-center bg-white min-w-xl gap-4 px-4 py-2">
+                    <div>読了済</div>
+                    <div>数量</div>
+                    <div>ページ</div>
                 </div>
             </div>
         </div>
