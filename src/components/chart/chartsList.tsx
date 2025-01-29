3988e2c4513f4fc2b4303a972f7790f99558bc95
taimats <beo8939@gmail.com>
2025-01-29 16:59:46 +0900

    修正:chartDateをuseStateで管理

--- src/components/chart/chartsList.tsx
+++ src/components/chart/chartsList.tsx
@@ -24,13 +24,27 @@ export const ChartList = () => {
         { year: "2024", month: "10月", value: "2"},
         { year: "2024", month: "11月", value: "2"},
         { year: "2024", month: "12月", value: "6"},
+        { year: "2023", month: "1月", value: "4"},
+        { year: "2023", month: "2月", value: "2"},
+        { year: "2023", month: "3月", value: "6"},
+        { year: "2023", month: "4月", value: "1"},
+        { year: "2023", month: "5月", value: "1"},
+        { year: "2023", month: "6月", value: "0"},
+        { year: "2023", month: "7月", value: "1"},
+        { year: "2023", month: "8月", value: "1"},
+        { year: "2023", month: "9月", value: "1"},
+        { year: "2023", month: "10月", value: "2"},
+        { year: "2023", month: "11月", value: "2"},
+        { year: "2023", month: "12月", value: "6"},
     ]
 
-    const [ targetYear, setTargetYear ] = useState("2024")
+    const thisYear = new Date().getFullYear.toString()
+    const [ targetYear, setTargetYear ] = useState(thisYear)
+    const [ filteredChartData, setfilteredChartData] = useState(chartData)
 
     const handleSelected = (selected: any) => {
         setTargetYear(selected)
-        chartData.filter((data) => (data.year === targetYear))
+        setfilteredChartData(filteredChartData.filter((data) => (data.year === targetYear)))
     }
     
     return (
@@ -43,7 +57,7 @@ export const ChartList = () => {
         </div>
         {labels.map((label, index) => (
             <div key={index} className="space-y-4">
-                <ChartBasis label={label} chartData={chartData}/>
+                <ChartBasis label={label} chartData={filteredChartData}/>
             </div>
         ))}
         </>
